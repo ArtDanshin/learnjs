69a055a6af20ef74b8b3c47f75c5d032d1f6023c
SilverDragoon <silvir007@gmail.com>
2016-04-05 00:01:46 +0300

    View friends in layout

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,7 @@
 var authButton = document.getElementsByClassName('save__button')[0],
     closeButton= document.getElementsByClassName('close-app')[0];
 
-
-
-authButton.addEventListener('click', VkOperations)
+authButton.addEventListener('click', itemList)
 
 closeButton.addEventListener('click', function() {
   VK.Auth.logout(function() {
@@ -31,7 +29,33 @@ var VkOperations = new Promise(function(resolve, reject) {
       console.log(response);
     })
     VK.api('friends.get', {'order' : 'random', 'fields' : 'photo_50'}, function(response) {
-      console.log(response)
+      console.log(response.response);
+      for (var i = 0; i < response.response.length; i++) {
+        var el = response.response[i];
+        document.getElementsByClassName('list__items')[0]
+                .appendChild(itemList(el.first_name,el.last_name,el.photo_50))
+      }
     })
   })
 })
+
+function itemList(firstName, lastName, photo) {
+  var fullName = firstName + ' ' + lastName;
+
+  var list__item = document.createElement('div');
+      list__item.className = 'list__item';
+  var list__avatar = document.createElement('img');
+      list__avatar.className = 'list__avatar';
+      list__avatar.setAttribute('src', photo);
+  var list__name = document.createElement('div');
+      list__name.className = 'list__name';
+      list__name.innerHTML = fullName;
+  var list__plus = document.createElement('div');
+      list__plus.className = 'list__plus';
+
+  list__item.appendChild(list__avatar);
+  list__item.appendChild(list__name);
+  list__item.appendChild(list__plus);
+
+  return list__item;
+}
