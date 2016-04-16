d7dd9e9a6111ede3581418091cccc7b180443677
SilverDragoon <silvir007@gmail.com>
2016-04-16 23:10:54 +0300

    Fix start servers node and gulp. Add gulp-develop-server plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,127 +1,140 @@
 /* ----- plugins ----- */
 
-var gulp 		= require('gulp'),
-	browserSync = require('browser-sync'),
-	jade        = require('gulp-jade'),
-	stylus 		= require('gulp-stylus');
-	concat		= require('gulp-concat')
+var gulp        = require('gulp'),
+    browserSync = require('browser-sync'),
+    server      = require('gulp-develop-server'),
+    jade        = require('gulp-jade'),
+    stylus      = require('gulp-stylus'),
+    browserify  = require('browserify'),
+    concat      = require('gulp-concat')
 
 /* -----  paths ----- */
 
 var paths = {
-		jade : {
-			location	: 'app/markups/**/*.jade',
-			compiled	: 'app/markups/_pages/*.jade',
-			destFolder	: 'dist/'
-		},
+    jade : {
+      location   : 'app/markups/**/*.jade',
+      compiled   : 'app/markups/_pages/*.jade',
+      destFolder : 'dist/'
+    },
 
-		stylus : {
-			mainStylFile: 'app/stylus/main.styl',
-			location		: 'app/stylus/**/*.styl',
-			destFolder	: 'dist/',
-		},
+    stylus : {
+      mainStylFile : 'app/stylus/main.styl',
+      location     : 'app/stylus/**/*.styl',
+      destFolder   : 'dist/',
+    },
 
-		fonts : {
-			location	: 'app/fonts/**/*',
-			destFolder	: 'dist/fonts/',
-		},
+    fonts : {
+      location    : 'app/fonts/**/*',
+      destFolder  : 'dist/fonts/',
+    },
 
-		js : {
-			location	: 'app/js/**/*.js',
-			destFile	: 'app.js',
-			destFolder	: 'dist/',
-		},
+    js : {
+      location   : 'app/js/**/*.js',
+      destFile   : 'app.js',
+      destFolder : 'dist/',
+    },
 
-		libjs : {
-			location	: ['node_modules/jquery/dist/jquery.min.js'],
-			destFile	: 'libs.js',
-			destFolder	: 'dist/',
-		},
+    libjs : {
+      location   : ['node_modules/jquery/dist/jquery.min.js'],
+      destFile   : 'libs.js',
+      destFolder : 'dist/',
+    },
 
-		images : {
-			location	: 'app/img/**/*',
-			destFolder	: 'dist/img/',
-		},
+    images : {
+      location   : 'app/img/**/*',
+      destFolder : 'dist/img/',
+    },
 
-		browserSync : {
-			baseDir		: 'dist',
-			watchPaths 	: ['dist/*.html', 'dist/*.css', 'dist/*.js']
-		},
+    server : {
+      location : 'app/server.js'
+    },
+
+    browserSync : {
+      baseDir    : 'dist',
+      watchPaths : ['dist/*.html', 'dist/*.css', 'dist/*.js']
+    },
 }
 
 /* ----- Jade ----- */
 
 gulp.task('jade', function() {
-	gulp.src(paths.jade.compiled)
-		.pipe(jade({
-			pretty: '\t',
-		}))
-		.pipe(gulp.dest(paths.jade.destFolder))
-		.pipe(browserSync.stream());
+  gulp.src(paths.jade.compiled)
+    .pipe(jade({
+      pretty: '\t',
+    }))
+    .pipe(gulp.dest(paths.jade.destFolder))
+    .pipe(browserSync.stream());
 });
 
 /* ----- Stylus ----- */
 
 gulp.task('stylus', function() {
-	gulp.src(paths.stylus.mainStylFile)
-		.pipe(stylus())
-		.pipe(gulp.dest(paths.stylus.destFolder));
+  gulp.src(paths.stylus.mainStylFile)
+    .pipe(stylus())
+    .pipe(gulp.dest(paths.stylus.destFolder));
 });
 
 /* ----- JS ----- */
 
 gulp.task('js', function() {
-	gulp.src(paths.js.location)
-		.pipe(concat(paths.js.destFile))
-		.pipe(gulp.dest(paths.js.destFolder));
+  gulp.src(paths.js.location)
+    .pipe(concat(paths.js.destFile))
+    .pipe(gulp.dest(paths.js.destFolder));
 });
 
 /* ----- LibsJS ----- */
 
 gulp.task('libjs', function() {
-	gulp.src(paths.libjs.location)
-		.pipe(concat(paths.libjs.destFile))
-		.pipe(gulp.dest(paths.libjs.destFolder));
+  gulp.src(paths.libjs.location)
+    .pipe(concat(paths.libjs.destFile))
+    .pipe(gulp.dest(paths.libjs.destFolder));
 });
 
 /* ----- Images ----- */
 
 gulp.task('images', function() {
-	gulp.src(paths.images.location)
-		.pipe(gulp.dest(paths.images.destFolder));
+  gulp.src(paths.images.location)
+    .pipe(gulp.dest(paths.images.destFolder));
 });
 
 /* ----- Fonts ----- */
 
 gulp.task('fonts', function() {
-	gulp.src(paths.fonts.location)
-		.pipe(gulp.dest(paths.fonts.destFolder));
+  gulp.src(paths.fonts.location)
+    .pipe(gulp.dest(paths.fonts.destFolder));
 });
 
 /* ----- Browser sync ----- */
 
 gulp.task('sync', function() {
-	browserSync.init({
-		port: 9000,
-		server: {
-			baseDir: paths.browserSync.baseDir
-		}
-	});
+  browserSync.init({
+    port: 9000,
+    server: {
+      baseDir: paths.browserSync.baseDir
+    }
+  });
+});
 
+/* ----- Develop Server ----- */
+
+gulp.task('server:start', function() {
+  server.listen({ 
+    path: paths.server.location 
+  });
 });
 
 /* ----- Watch ----- */
 
 gulp.task('watch', function(){
-	gulp.watch(paths.jade.location, ['jade']);
-	gulp.watch(paths.stylus.location, ['stylus']);
-	gulp.watch(paths.js.location, ['js']);
-	gulp.watch(paths.browserSync.watchPaths).on('change', browserSync.reload);
+  gulp.watch(paths.jade.location, ['jade']);
+  gulp.watch(paths.stylus.location, ['stylus']);
+  gulp.watch(paths.js.location, ['js']);
+  gulp.watch(paths.browserSync.watchPaths).on('change', browserSync.reload);
 });
 
 /* ----- Default ----- */
 
-gulp.task('default', ['jade', 'stylus', 'js', 'libjs', 'fonts', 'images', 'sync', 'watch']);
+gulp.task('default', ['jade', 'stylus', 'js', 'libjs', 'fonts', 'images', 'sync', 'server:start', 'watch']);
 
 /* ----- Build Task -----*/
 
